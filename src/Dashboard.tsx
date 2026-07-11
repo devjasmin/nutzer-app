e5f5e985d80c2d84cc1b52ce9384627c4cd706f2
Jasmin <j.longa@gmx.ch>
2026-07-11 14:55:56 +0200

    Vite Projekte mit React erstellt. Header erstellt

---
+++ src/Dashboard.tsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <section className="header">
+      <h1>Nutzer App</h1>
+      <h3>Willkommen zur Nutzer App für Calisthenics</h3>
+    </section>
+  );
+}
+
+export default Dashboard;
